076103273374650aa671478b386ebed29f82c8fd
Joey <meijerjoey@gmail.com>
2022-04-17 15:34:07 +0200

    Initial commit

---
+++ premake5.lua
@@ -0,0 +1,140 @@
+workspace "Typhoon"
+	architecture "x86_64"
+	startproject "Convection"
+
+	configurations 
+	{
+		"Debug", 
+		"Release", 
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Include directories relative to root folder (solution directory)
+IncludeDir = {}
+IncludeDir["GLFW"]  	  = "vendor/GLFW/include"
+IncludeDir["ImGui"] 	  = "vendor/imgui"
+IncludeDir["stb_image"]   = "vendor/stb_image"
+
+group "Dependencies"
+	include "Typhoon/vendor/GLFW"
+	include "Typhoon/vendor/imgui"
+group ""
+
+group "Core"
+project "Typhoon"
+	location "Typhoon"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "typhpch.h"
+	pchsource "Typhoon/src/typhpch.cpp"
+
+	files 
+	{
+		"%{prj.name}/src/**.h", 
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/stb_image/**.h",
+		"%{prj.name}/vendor/stb_image/**.cpp",
+		"%{prj.name}/vendor/glm/glm/**.hpp",
+		"%{prj.name}/vendor/glm/glm/**.inl"
+	}
+
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS",
+		"GLFW_INCLUDE_NONE"
+	}
+
+	includedirs 
+	{
+		"%{prj.name}/src", 
+		"%{prj.name}/vendor/spdlog/include",
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.Glad}",
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.stb_image}"
+	}
+
+	links
+	{
+		"GLFW",
+		"Glad",
+		"ImGui",
+		"opengl32.lib"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		defines "TYPH_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "TYPH_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "TYPH_DIST"
+		runtime "Release"
+		optimize "on"
+group ""
+
+group "Client"
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.h", 
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs 
+	{
+		"Typhoon/vendor/spdlog/include", 
+		"Typhoon/src",
+		"Typhoon/vendor",
+		"%{IncludeDir.glm}"
+	}
+
+	links 
+	{
+		"Typhoon"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		defines "TYPH_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "TYPH_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "TYPH_DIST"
+		runtime "Release"
+		optimize "on"
+group ""
